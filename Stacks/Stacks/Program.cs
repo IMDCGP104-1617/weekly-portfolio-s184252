70aacef7caadb8de6ea2b7d853cf079c9fd7d114
s184252 <s184252@ucs.ac.uk>
2016-10-28 11:12:11 +0100

    Stacks Portfolio

---
+++ Stacks/Stacks/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stacks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack myStack = new Stack();
+
+            for(int i = 0; i < 50; i++)
+            {
+                myStack.StackPush(i);
+            }
+
+            myStack.StackPush((int)300);
+            myStack.StackPop();
+
+            myStack.StackPrint();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Stacks/Stacks/Stack.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stacks
+{
+    class Stack
+    {
+        public class Item
+        {
+            public object Content;
+            public Item Next;
+        }
+
+        public Stack()
+        {
+            count = 0;
+            top = null;
+        }
+
+        private Item top = null;
+        private int count = 0;
+
+        public void StackPush(object content)
+        {
+            Item newItem = new Item();
+            newItem.Content = content;
+
+            if(top == null)
+            {
+                top = newItem;
+            }
+            else
+            {
+                Item tempItem = top;
+                top = newItem;
+                newItem.Next = tempItem;
+            }
+
+            count++;
+        }
+
+        public object StackPop()
+        {
+            if(top == null)
+            {
+                System.Console.WriteLine("Stack is empty");
+                return null;
+            }
+            else
+            {
+                Item tempItem = top;
+                top = tempItem.Next;
+                count--;
+                return tempItem.Content;
+            }
+        }
+
+        public bool isEmpty()
+        {
+            if(top == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public object StackPeek()
+        {
+            return top.Content;
+        }
+
+        public int StackCount()
+        {
+            return count;
+        }
+
+        public void StackPrint()
+        {
+            Item tempItem = top;
+
+            while(tempItem != null)
+            {
+                System.Console.WriteLine(tempItem.Content);
+                tempItem = tempItem.Next;
+            }
+        }
+
+        ~Stack()
+        {
+            while(top != null)
+            {
+                top = top.Next;
+            }
+        }
+    }
+}
